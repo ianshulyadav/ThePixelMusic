acde454b917a0e89456f0f6962e272ceee6d162a
ianshulyadav <ianshul.yadavv@gmail.com>
2026-07-02 00:35:19 +0530

    Extend LibraryPlaylistCard width to 280.dp so long titles do not wrap awkwardly under Your Library

--- app/src/main/java/com/unshoo/pixelmusic/presentation/screens/ExploreScreen.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/screens/ExploreScreen.kt
@@ -816,7 +816,7 @@ fun LibraryPlaylistCard(
     
     Card(
         modifier = Modifier
-            .width(220.dp)
+            .width(280.dp)
             .height(130.dp)
             .clip(shape)
             .clickable(onClick = onClick),
